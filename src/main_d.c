ef391d2196f63a39cd359efe8832eb13a2d05444
CosmicSpaceGoose <dim13@i.ua>
2018-10-16 18:36:51 +0300

    g_16_h

--- src/main_d.c
+++ src/main_d.c
@@ -1,4 +1,16 @@
-int main()
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
 {
+	int sck;
+
+	if (argc > 1)
+		write(1, argv[1], 3);
+	else
+	{
+		sck = socket(AF_INET, SOCK_RAW, )
+	}
 	return (0);
 }
